d4818b7389cc1a403df6db445b540e8183fb119d
louishsu <is.louishsu@foxmail.com>
2019-07-11 16:55:37 +0800

    Time: 2019-07-11 16:55:37

--- main.py
+++ main.py
@@ -91,12 +91,6 @@ def main_unsupervised(num_classes, feature_size):
     trainer.train()
     trainer.show_embedding_features(validset)
 
-if __name__ == "__main__":
-    main_cosmargin()
-    main_spheremargin()
-    main_arcmargin()
-    exit(0)
-
 
 if __name__ == "__main__":
     import argparse

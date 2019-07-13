8f22351107c753f47c7dfb2d4dbb918aa843e742
louishsu <is.louishsu@foxmail.com>
2019-07-13 08:55:22 +0800

    config

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ configer.logdir = './log'
 
 configer.inputsize = (1, 28, 28)    # (C, H, W)
 configer.batchsize = 256
-configer.n_epoch = 150
+configer.n_epoch = 200
 configer.valid_freq = 1
 
-configer.lrbase = 0.001
-configer.adjstep = [130,]
+configer.lrbase = 0.0005
+configer.adjstep = [120, 170]
 configer.gamma = 0.1
 
 configer.cuda = True

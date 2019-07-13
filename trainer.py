7381d80647825f3b075375093d54bc7389c7d8b9
louishsu <is.louishsu@foxmail.com>
2019-07-13 20:51:53 +0800

    trainer

--- trainer.py
+++ trainer.py
@@ -348,7 +348,7 @@ class MarginTrainer(SupervisedTrainer):
             duration_time = time.time() - start_time
             start_time = time.time()
 
-        if self.show_embedding and ((self.cur_epoch - 1) % 10 == 0):
+        if self.show_embedding and ((self.cur_epoch - 1) % 3 == 0):
             self.writer.add_embedding(mat, metadata, global_step=self.cur_epoch)
 
             mat = mat.cpu().detach().numpy()
@@ -467,7 +467,7 @@ class MarginTrainerWithParameter(SupervisedTrainer):
             duration_time = time.time() - start_time
             start_time = time.time()
 
-        if self.show_embedding and ((self.cur_epoch - 1) % 10 == 0):
+        if self.show_embedding and ((self.cur_epoch - 1) % 3 == 0):
             self.writer.add_embedding(mat, metadata, global_step=self.cur_epoch)
 
             mat = mat.cpu().detach().numpy()

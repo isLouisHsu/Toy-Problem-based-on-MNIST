8232df6ebaa149a3350a40347d67605a9bfa71fd
louishsu <is.louishsu@foxmail.com>
2019-08-03 11:53:06 +0800

    baseline

--- trainer.py
+++ trainer.py
@@ -883,7 +883,7 @@ class UnsupervisedTrainer():
         if self.show_embedding:
             mat = torch.cat([mat, self.criterion.m], dim=0)
             tens = torch.ones(1, self.criterion.m.shape[0])*10
-            if torch.cuda.is_available() and configer.cuda:
+            if torch.cuda.is_available() and self.configer.cuda:
                 tens = tens.cuda()
             metadata = torch.cat([mat, tens], dim=0)
             self.writer.add_embedding(mat, metadata, global_step=self.cur_epoch)
